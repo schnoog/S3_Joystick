a65aa96c3ce79976c591c8a937e8e6aa05ed75e8
schnoog <schnoog@schnoog.eu>
2023-04-11 20:48:13 +0200

    another step

--- src/inc_joytick.h
+++ src/inc_joytick.h
@@ -1,4 +1,10 @@
 #include "Joystick_ESP32S2.h"
+#include <esp_task_wdt.h>
+#include <esp_heap_caps.h>
+
+int UpdatedData = 0;
+uint16_t valueToF = 0;
+SemaphoreHandle_t xMutex = NULL;  // Create a mutex object
 
 // Create Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
@@ -13,15 +19,23 @@ const bool testAutoSendMode = false;
 
 void MyButtonTest() 
 {
+  int sc  = 0;
+  Serial.print("Button: ");
   for (int button = 0; button < 127; button++)
   {
-        Serial.print("Press button");
-        Serial.println(button);
+        Serial.print(" ");
+        sc++;
+        if (sc > 31){
+          sc = 0;
+          Serial.println(button);
+        }else{
+          Serial.print(button);
+        }
+        
+        
         Joystick.pressButton(button);
         Joystick.sendState();
         delay(500);
-        Serial.print("Release button");
-        Serial.println(button);
         Joystick.releaseButton(button);
         
   }
@@ -34,7 +48,7 @@ void MyButtonTest()
 
 
 void joystick_setup() {
-
+  xMutex = xSemaphoreCreateMutex();
   // Set Range Values
   Joystick.setXAxisRange(0, 65535);
   Joystick.setYAxisRange(0, 65535);
@@ -60,3 +74,49 @@ void joystick_setup() {
 void joystick_loop(){
   MyButtonTest();
 }
+
+void testpress_button(int button){
+              Joystick.pressButton(button);
+              Joystick.sendState();
+              vTaskDelay(100);
+              Serial.print(".");
+              Joystick.releaseButton(button);
+              Joystick.sendState();
+}
+
+void joystick_infini_loop(void *pvParameters){
+  int resetcount = 0;
+  esp_task_wdt_init(5, false);
+  esp_task_wdt_add(NULL);
+  while(1){
+        resetcount++;
+        
+        Serial.print("Btn: ");
+        Serial.print(" ");
+        Serial.print(resetcount);
+        Serial.print(" ");
+        for (int button = 0; button < 127; button++)
+        {
+          
+                  
+
+                  if (xSemaphoreTake (xMutex, portMAX_DELAY)) {  // take the mutex
+                    //Serial.println("xmute ok");
+                      testpress_button(button);
+                      vTaskDelay(10);
+                       xSemaphoreGive (xMutex);  // release the mutex
+                  }else{
+                    Serial.println("xmute failed");
+                    vTaskDelay(10);
+                  }
+                  
+                  esp_task_wdt_reset();
+//                  Serial.println(esp_task_wdt_reset());
+//                  heap_caps_print_heap_info(MALLOC_CAP_8BIT);                
+                  //ESP.getHeapFragmentation() / ESP.getMaxFreeBlockSize()
+              }
+        Serial.println("");
+      
+        }
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,91 @@
+
+
+//#include <HardwareSerial.h>
+//#include "tasking.h"
+
+#define USE_MUTEX
 #include "inc_joytick.h"
 
 
-void setup(){
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  joystick_setup();
-}
 
+
+
+
+
+int loopnum = 0;
+
+
+uint32_t HS = 0;
+uint32_t FH = 0;
+uint32_t MFH = 0;
+uint32_t MAH = 0;
+
+uint32_t CHS = 0;
+uint32_t CFH = 0;
+uint32_t CMFH = 0;
+uint32_t CMAH = 0;
+
+
+
+
+
+
+void setup(){
+  static int task_number0 = 0;
+  static int task_number1 = 0;
+  Serial.begin(115200);
+  delay(100);
+  //Serial1.begin(115200);
+  Serial.setDebugOutput(true);
+  delay(5000);
+  
+  
+  joystick_setup();
+  Serial.println("Starting up");
+  xTaskCreate (joystick_infini_loop,"testloop",2500,(void*)&task_number0,1,NULL);
+  //joystick_loop
+  HS = ESP.getHeapSize(); //total heap size
+  FH = ESP.getFreeHeap(); //available heap
+  MFH = ESP.getMinFreeHeap(); //lowest level of free heap since boot
+  MAH = ESP.getMaxAllocHeap(); //largest block of heap that can be allocated at once
+
+
+}
 
 void loop(){
-  joystick_loop();
+    
 }
+
+void loopX(){
+  loopnum++;
+  //joystick_loop();
+////  vTaskDelay(10);
+Serial.println("");
+Serial.print("getHeapSize       ");
+Serial.print(ESP.getHeapSize());
+Serial.print(" / ");
+Serial.println(HS);
+
+Serial.print("getFreeHeap       ");
+Serial.print(ESP.getFreeHeap());
+Serial.print(" / ");
+Serial.println(FH);
+
+Serial.print("getMinFreeHeap    ");
+Serial.print(ESP.getMinFreeHeap());
+Serial.print(" / ");
+Serial.println(MFH);
+
+Serial.print("getMaxAllocHeap   ");
+Serial.print(ESP.getMaxAllocHeap());
+Serial.print(" / ");
+Serial.println(MAH);
+
+
+Serial.println("-----------------------------------------");
+
+Serial.print("Cycle :");
+Serial.println(loopnum);
+MyButtonTest() ;
+}
+
